4c9d4c4464a09c1e305cd97ddc16918bfbeccda1
Sebastian Rosengren <sebastian@rosengren.me>
2023-09-08 16:23:53 +0200

    Added mock conference service, and Now.cshtml

--- Program.cs
+++ Program.cs
@@ -7,20 +7,20 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
-builder.Services.AddDbContext<SwetuggContext>(options => 
+builder.Services.AddDbContext<SwetuggContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("Swetugg"))
   .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
 
-builder.Services.AddScoped<ConferenceService>();
+builder.Services.AddScoped<IConferenceService, ConferenceService>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+  app.UseExceptionHandler("/Error");
+  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+  app.UseHsts();
 }
 
 app.UseHttpsRedirection();
